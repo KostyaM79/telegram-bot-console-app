f422f110e28ca5ca3d7fa254e7a8da24544b08db
KostyaM79 <84287360+KostyaM79@users.noreply.github.com>
2022-08-29 18:33:43 +0300

    Update Program.cs
    Снова убрал ожидание.

--- TelegramBotConsoleApp/Program.cs
+++ TelegramBotConsoleApp/Program.cs
@@ -11,7 +11,6 @@ namespace TelegramBotConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Всем привет!");
-            Console.ReadKey();
         }
     }
 }
