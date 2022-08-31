5477c01a6bdd82cfe398f43f94e5a45c854982a6
Konstantin Makhnutin <mahnutin@mail.ru>
2022-08-31 22:42:11 +0300

    Добавлен функционал: сохранение файлов, отправка файлов

--- TelegramBotConsoleApp/Program.cs
+++ TelegramBotConsoleApp/Program.cs
@@ -45,14 +45,28 @@ namespace TelegramBotConsoleApp
                 {
                     var message = update.Message;
 
+                    Console.WriteLine($"Тип сообщения: {message.Type}\nID: {message.From.Id}\nПользователь: {message.From.FirstName}\n");
+
                     switch(message.Type)
                     {
                         case MessageType.Text:
                             TextMessageHandler(message);
                             break;
 
+                        case MessageType.Audio:
+                            SaveAudio(message);
+                            break;
+
+                        case MessageType.Voice:
+                            SaveVoice(message);
+                            break;
+
                         case MessageType.Document:
-                            SaveFile(message);
+                            SaveDocument(message);
+                            break;
+
+                        case MessageType.Photo:
+                            SavePhoto(message);
                             break;
                     }
                 }
@@ -80,18 +94,23 @@ namespace TelegramBotConsoleApp
         /// <param name="message"></param>
         private static void CommandHandler(Message message)
         {
-            switch (message.Text)
+            string[] parametrs = message.Text.Split(' ');
+
+            switch (parametrs[0])
             {
                 case "/start":
-                    bot.SendTextMessageAsync(message.Chat.Id, "Начнём, пожалуй.");
+                    SendStartMessage(message);
                     break;
 
                 case "/get_files":
                     SendFileList(message);
                     break;
 
-                case "/exit":
-                    bot.SendTextMessageAsync(message.Chat.Id, "Пока!");
+                case "/load":
+                    if (parametrs.Length == 2)
+                        UploadFile(parametrs[1], message);
+                    else
+                        bot.SendTextMessageAsync(message.From.Id, "Неверное имя файла!");
                     break;
             }
         }
@@ -100,14 +119,11 @@ namespace TelegramBotConsoleApp
         /// Сохраняет полученные файлы
         /// </summary>
         /// <param name="message"></param>
-        private static async void SaveFile(Message message)
+        private static async void SaveFile(string fileName, string fileId, long chatId)
         {
-            string directoryPath = System.IO.Directory.CreateDirectory($"files\\{message.Chat.Id}").FullName;   //Создаём папку и получаем её путь
-            string fileName = message.Document.FileName;                                                        //Получаем имя файла
-            string fileId = message.Document.FileId;                                                            //Получаем ID файла
-            long chatId = message.Chat.Id;                                                                      //Получаем ID пользователя
+            string directoryPath = System.IO.Directory.CreateDirectory($"files\\{chatId}").FullName;   //Создаём папку и получаем её путь
 
-            var file = await bot.GetFileAsync(fileId);
+            Telegram.Bot.Types.File file = await bot.GetFileAsync(fileId);
             System.IO.FileStream fs = new System.IO.FileStream($"{directoryPath}\\{fileName}", System.IO.FileMode.Create);
             await bot.DownloadFileAsync(file.FilePath, fs);
             fs.Close();
@@ -137,6 +153,77 @@ namespace TelegramBotConsoleApp
             }
 
             bot.SendTextMessageAsync(message.Chat.Id, $"Список файлов:\n\n{sb.ToString()}");
+        }
+
+        private static void SaveAudio(Message message)
+        {
+            SaveFile(message.Audio.FileName, message.Audio.FileId, message.From.Id);
+        }
+
+        private static void SaveDocument(Message message)
+        {
+            SaveFile(message.Document.FileName, message.Document.FileId, message.From.Id);
+        }
+
+        private static async void SaveVoice(Message message)
+        {
+            //SaveFile(CreateVoiceFileName(message.From.Id), message.Voice.FileId, message.From.Id);
+
+            Telegram.Bot.Types.File file = await bot.GetFileAsync(message.Voice.FileId);
+            string fileName = file.FilePath.Split('/')[1];
+            SaveFile(fileName, file.FileId, message.From.Id);
+        }
+
+        private static async void SavePhoto(Message message)
+        {
+            Telegram.Bot.Types.File file = await bot.GetFileAsync(message.Photo[message.Photo.Length - 1].FileId);
+            string fileName = file.FilePath.Split('/')[1];
+            SaveFile(fileName, file.FileId, message.From.Id);
+        }
+
+        private static async void UploadFile(string fileName, Message message)
+        {
+            string filePath = $"files\\{message.From.Id}\\{fileName}";
+            string[] nameComponents = fileName.Split('.');
+
+            if (System.IO.File.Exists(filePath))
+            {
+                switch (nameComponents[nameComponents.Length - 1])
+                {
+                    case "jpg":
+                        await SendDocument(filePath, fileName, message);
+                        break;
+
+                    case "oga":
+                        await SendVoice(filePath, fileName, message);
+                        break;
+                }
+            }
+        }
+
+        private static async Task SendDocument(string filePath, string fileName, Message message)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            Message mess = await bot.SendDocumentAsync(message.From.Id, new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs, fileName));
+            fs.Close();
+            fs.Dispose();
+        }
+
+        private static async Task SendVoice(string filePath, string fileName, Message message)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            Message mess = await bot.SendVoiceAsync(message.From.Id, new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs, fileName));
+            fs.Close();
+            fs.Dispose();
+        }
+
+        private static async Task SendStartMessage(Message message)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Привет, {message.From.Username}!\nЯ телеграмм-бот, меня зовут Старый пёс.\nЯ могу схранять ваши файлы, выводить их список и отправлять их обратно по запросу.\n\n");
+            sb.Append($"Чтобы я сохранил ваш файл, просто отправьте мне его.\nЧтобы получить список ваших файлов, введите команду /get_files\n");
+            sb.Append($"Чтобы загрузить файл, введите команду /load имя_файла");
+            await bot.SendTextMessageAsync(message.From.Id, sb.ToString());
         }
     }
 }
